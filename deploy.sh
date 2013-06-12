b9f564759c6d591632fb4f8a35d401cc8550658d
Matt Bornski <matt@bornski.com>
2013-06-12 12:28:28 -0400

    Need to bundle install and then check in gemfile.lock Fix some Heroku deployment problems

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 
 cd $(dirname $0)
 
-heroku create
+heroku apps:info
+if [ "$?" != "0" ] ; then
+	git remote rm heroku
+	heroku apps:create $(basename $PWD | tr '[A-Z]' '[a-z]')
+fi
 git push heroku master
 heroku ps:scale web=1
 heroku open
